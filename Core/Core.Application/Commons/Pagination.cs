57fb4a708ebc2f0ba303f81a380db6c3073d72a1
giorgi.gelashvili <g.gelashvili02@outlook.com>
2024-04-22 19:04:38 +0400

    გაუმჯობესდა Pagination კლასი

--- Core/Core.Application/Commons/Pagination.cs
+++ Core/Core.Application/Commons/Pagination.cs
@@ -5,14 +5,14 @@ public sealed class Pagination<T>
 {
     public IEnumerable<T> Items { get; private set; }
 
-    public int PageIndex { get; private set; }
-    public int PageSize { get; private set; }
+    public long PageSize { get; private set; }
+    public long PageIndex { get; private set; }
 
-    public long TotalPages { get; private set; }
     public long TotalCount { get; private set; }
+    public long TotalPages => (long)Math.Ceiling((double)this.TotalCount / this.PageSize);
 
     public bool HasPreviousPage => PageIndex > 1;
-    public bool HasNextPage => PageIndex < TotalPages;
+    public bool HasNextPage => this.PageIndex < this.TotalPages;
 
 
 
@@ -21,12 +21,12 @@ public sealed class Pagination<T>
     public Pagination() { /* for deserialization "Mapster" */ }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
-    public Pagination(IEnumerable<T> items, long count, int pageIndex, int pageSize)
+    public Pagination(IEnumerable<T> items, long count, long pageIndex, long pageSize)
     {
         this.PageIndex = pageIndex;
         this.PageSize = pageSize;
-        this.TotalPages = (long)Math.Ceiling(count / (double)pageSize);
         this.TotalCount = count;
+
         this.Items = items;
     }
 
