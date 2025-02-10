2784e4ddaf6712210c306b138090792bd2f6a39e
giorgi.gelashvili <g.gelashvili02@gmail.com>
2025-02-10 16:15:49 +0400

    დაემატა JsonCompressor, რომელიც BLOB ად აქცევს JSON ს და საკმაოდ ამცირებს ზომაში

---
+++ Core/Core.Domain/Shared/JsonCompressor.cs
@@ -0,0 +1,25 @@
+﻿using System.IO.Compression;
+using System.Text;
+
+namespace Core.Domain.Shared;
+public static class JsonCompressor
+{
+    public static byte[] Compress(string json)
+    {
+        using var outputStream = new MemoryStream();
+        using (var brotliStream = new BrotliStream(outputStream, CompressionLevel.Optimal))
+        {
+            var jsonBytes = Encoding.UTF8.GetBytes(json);
+            brotliStream.Write(jsonBytes, 0, jsonBytes.Length);
+        }
+        return outputStream.ToArray();
+    }
+
+    public static string Decompress(byte[] compressedJson)
+    {
+        using var inputStream = new MemoryStream(compressedJson);
+        using var brotliStream = new BrotliStream(inputStream, CompressionMode.Decompress);
+        using var reader = new StreamReader(brotliStream);
+        return reader.ReadToEnd();
+    }
+}
