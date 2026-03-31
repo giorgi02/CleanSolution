47636ad2beae42b077f4357d3c7719f17f49f2ce
giorgi.gelashvili <g.gelashvili02@gmail.com>
2026-03-31 17:05:48 +0400

    Add DateTime extension for age calculation

---
+++ Core/Core.Shared/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Shared.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static int CalculateAge(this DateTime birthDate)
+    {
+        var today = DateTime.Today;
+
+        if (birthDate.Date > today)
+            throw new ArgumentException("Birth date cannot be in the future.", nameof(birthDate));
+
+        var age = today.Year - birthDate.Year;
+        if (birthDate.Date > today.AddYears(-age)) age--;
+
+        return age;
+    }
+}
